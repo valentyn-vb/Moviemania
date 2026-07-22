070eaa644a2b056f9f58336189bed526d4076538
Valentyn Beznosko <valikbeznosko@gmail.com>
2026-07-22 21:25:09 +0200

    move components to new folders

--- src/components/CrewMember/CrewMember.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Wraper = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  gap: ${p => p.theme.space[5]}px;
-  padding-top: ${p => p.theme.space[4]}px;
-  padding-bottom: ${p => p.theme.space[4]}px;
-`;
--- src/components/Layout/Layout.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Wraper = styled.div`
-  @media screen and (min-width: ${p => p.theme.sizes.pc}) {
-    display: grid;
-    grid-template-columns: minmax(256px, 290px) 5fr;
-    height: 100vh;
-  }
-`;
